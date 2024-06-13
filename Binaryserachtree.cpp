10c1b9837b974ffbdd7b266abe4fdafe0e8d0d23
anobee <HP@DESKTOP-KFAVCD8>
2024-06-13 15:19:59 +0700

    membuat funsi untuk node baru

--- Binaryserachtree.cpp
+++ Binaryserachtree.cpp
@@ -22,6 +22,21 @@ class binarytree
 {
     public:
     node* root;
-    binary tree()
+    binarytree()
     {
         root = nullptr; //inisialisasi root sebagai titik null
+    }
+    void insert(string element) //memasukan sebuah node ke dalam binary tree
+    {
+        node*newnode = new node(element, nullptr, nullptr); //mengalokasikan node baru 
+        newnode->info = element;
+        newnode->lchild = nullptr;
+        newnode->rchild = nullptr;
+
+        node* parent = nullptr;
+        node* currentnode = nullptr;
+        search(element, parent, currentnode);
+
+        if (parent = nullptr)
+    }
+}
