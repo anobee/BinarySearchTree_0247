4fa51e7b08fc5758a82d05cb7890cb831abd5d2f
anobee <HP@DESKTOP-KFAVCD8>
2024-06-13 15:11:36 +0700

    membuat class node

---
+++ Binaryserachtree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class node
+{
+    public:
+        string info;
+        string* lchild;
+        string* rchild;
+
+        //konstruksi buat kelas
+        node(string i, node* l, node* r);
+        {
+            info = i;
+            lchild = l;
+            rchild = r;
+        }
+};
+
+class binarytree
+{
+    
+}
